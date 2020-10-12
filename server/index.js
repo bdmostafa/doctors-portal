a954e4735753318087dca9ae7d72fd1ef2bd526c
bdmostafa <mdmostafaalmahmud@gmail.com>
2020-10-12 07:05:49 +0600

    addAppointment api

--- server/index.js
+++ server/index.js
@@ -13,11 +13,55 @@ app.use(cors())
 
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.USER_PASS}@cluster0.efifc.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(
+    uri,
+    {
+        useUnifiedTopology: true,
+        useNewUrlParser: true
+    }
+);
+
+client.connect(err => {
+    const appointmentsCollection = client.db(process.env.DB_NAME).collection(process.env.DB_COLLECTION);
+    app.post('/addAppointment', (req, res) => {
+        const appointment = req.body;
+
+        appointmentsCollection.insertOne(appointment)
+        .then(result => {
+            res.send(result.insertedCount > 0)
+        })
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //   client.close();
+});
+
+
 
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
-  
+    res.send('Hello World! doctros portal')
+})
+
 app.listen(port)
