7bfa4022716b5be30a5ac4fbd95a3c377d04dc56
bdmostafa <mdmostafaalmahmud@gmail.com>
2020-10-12 22:28:38 +0600

    hide some sidebar links from a normal user using fetch api /isDoctor

--- client/src/components/DashboardPage/Sidebar/Sidebar.js
+++ client/src/components/DashboardPage/Sidebar/Sidebar.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { UserContext } from '../../../App';
 
 const Sidebar = () => {
+    const { loggedInUser, setLoggedInUser } = useContext(UserContext);
+    const [isDoctor, setIsDoctor] = useState(false);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/isDoctor', {
+            method: 'POST',
+            headers: { 'Content-type': 'application/json' },
+            body: JSON.stringify({ email: loggedInUser.email })
+        })
+            .then(res => res.json())
+            .then(data => setIsDoctor(data));
+    }, [])
+
     const listStyle = {
-        listStyle: 'none', 
+        listStyle: 'none',
         textDecoration: 'none',
-        marginTop: '5rem', 
+        marginTop: '5rem',
         display: 'inline-grid',
     }
     return (
@@ -13,10 +27,16 @@ const Sidebar = () => {
             <ul style={listStyle}>
                 <Link to='/dashboard' >Dashboard</Link >
                 <Link to='/dashboard/appointments'>Appointments</Link >
-                <Link to='/dashboard/allPatients'>Patients</Link >
-                <Link to='/dashboard/prescriptions'>Prescriptions</Link >
-                <Link to='/dashboard/addDoctor'>Add Doctor</Link >
-                <Link to='/dashboard'>Settigns</Link >
+                {
+                    isDoctor
+                    && <>
+                        <Link to='/dashboard/allPatients'>Patients</Link >
+                        <Link to='/dashboard/prescriptions'>Prescriptions</Link >
+                        <Link to='/dashboard/addDoctor'>Add Doctor</Link >
+                        <Link to='/dashboard'>Settigns</Link >
+                    </>
+                }
+
             </ul>
             <div>
                 <Link to="/"> <span>Logout</span></Link>
