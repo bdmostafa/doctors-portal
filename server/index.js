50693553fe48f3426f1bb5000eae210389f980ad
bdmostafa <mdmostafaalmahmud@gmail.com>
2020-10-13 13:01:05 +0600

    addDoctor api with file system express fileupload

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const uploadFile = require('express-fileupload');
 require('dotenv').config();
+const fs = require('fs-extra');
 
 const port = 5000;
 
@@ -45,21 +46,21 @@ client.connect(err => {
         const email = req.body.email;
 
         doctorsCollection.find({ email })
-        .toArray((err, doctors) => {
-            const filter = { date }
-            if(doctors.length === 0) {
-                // If doctor email is not found,
-                // email is added to filter
-                filter.email = email;
-            }
-            // find the collections matching date and only loggedInUser email
-            appointmentsCollection.find(filter)
-            .toArray((err, documents) => {
-                console.log(date, email, doctors, documents)
-                res.send(documents)
-                // console.log(documents)
+            .toArray((err, doctors) => {
+                const filter = { date }
+                if (doctors.length === 0) {
+                    // If doctor email is not found,
+                    // email is added to filter
+                    filter.email = email;
+                }
+                // find the collections matching date and only loggedInUser email
+                appointmentsCollection.find(filter)
+                    .toArray((err, documents) => {
+                        console.log(date, email, doctors, documents)
+                        res.send(documents)
+                        // console.log(documents)
+                    })
             })
-        })
     })
 
     // This API is used for checking if an user is a doctor or not
@@ -67,9 +68,9 @@ client.connect(err => {
         const email = req.body.email;
 
         doctorsCollection.find({ email })
-        .toArray((err, doctors) => {
-            res.send(doctors.length > 0)
-        })
+            .toArray((err, doctors) => {
+                res.send(doctors.length > 0)
+            })
     })
 
 
@@ -81,29 +82,53 @@ client.connect(err => {
             })
     })
 
-app.post('/addDoctor', (req, res)=> {
-    const file = req.files.file;
-    console.log(file, req.body)
+    // API for adding a doctor with profile picture
+    app.post('/addDoctor', (req, res) => {
+        const file = req.files.file;
+        // console.log(file)
 
         const fileName = file.name;
-        file.mv(`${__dirname}/doctors/${fileName}`, err => {
+        const filePath = `${__dirname}/doctors/${fileName}`
+        file.mv(filePath, err => {
             if (err) {
                 console.log(err);
-                return res.status(500).send({msg: 'Failed to upload Image'});
+                res.status(500).send({ msg: 'Failed to upload Image' });
             }
             // return res.send({name: fileName})
+            const newImg = fs.readFileSync(filePath);
+            const encodedImg = newImg.toString('base64');
+
+            const image = {
+                // description: req.body.description,
+                contentType: file.mimetype,
+                size: file.size,
+                img: Buffer(encodedImg, 'base64')
+            };
+
+            const totalData = JSON.parse(req.body.total)
+            // totalData.image = fileName;
+            totalData.image = image;
+            // console.log(totalData)
+            doctorsCollection.insertOne(totalData)
+                .then(result => {
+                    fs.remove(filePath, err => {
+                        if (err) console.log(err);
+                        res.send(result.insertedCount > 0);
+                    })
+                    // res.send(result.insertedCount > 0);
+                })
         })
 
-        const totalData = JSON.parse(req.body.total)
-        totalData.image = fileName;
 
-        doctorsCollection.insertOne(totalData)
-        .then(result => {
-            res.send(result.insertedCount > 0)
-        })
-})
+    })
 
-
+    app.get('/doctors', (req, res) => {
+        doctorsCollection.find({})
+            .toArray((err, doctors) => {
+                console.log(err, doctors)
+                res.send(doctors);
+            })
+    })
 
 
 
