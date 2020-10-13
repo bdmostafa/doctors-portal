62d5440edecebbfa0d796b5bca0eedb29551e54d
bdmostafa <mdmostafaalmahmud@gmail.com>
2020-10-13 13:10:16 +0600

    react-preloaders

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Lines } from 'react-preloaders';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Lines />
   </React.StrictMode>,
   document.getElementById('root')
 );
